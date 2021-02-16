86dea557ac01788502ea4825bf37a79a7fb9377b
majrbek kotto <dewdar@live.no>
2021-02-16 22:10:49 +0100

    Content projection

--- cinemahub/src/app/utilities/generic-list/generic-list.component.ts
+++ cinemahub/src/app/utilities/generic-list/generic-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-generic-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./generic-list.component.css']
 })
 export class GenericListComponent implements OnInit {
+  @Input()
+  list;
 
   constructor() { }
 
