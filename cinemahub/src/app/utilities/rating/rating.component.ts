4643eeecfc722d1b2d39215c602ce156a0197398
majrbek kotto <dewdar@live.no>
2021-02-17 19:59:17 +0100

    Rating system with ngClass complete

--- cinemahub/src/app/utilities/rating/rating.component.ts
+++ cinemahub/src/app/utilities/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingComponent implements OnInit {
 
+  @Input()
+  maxRating = 5;
+
+  @Input()
+  selectedRate = 0;
+  previousRate = 0;
+
+  maxRatingArray = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.maxRatingArray = Array(this.maxRating).fill(0);
+  }
+
+  handleMouseEnter(index: number){
+    this.selectedRate = index;
+  }
+
+  handleMouseLeave(){
+    if(this.previousRate !== 0){
+      this.selectedRate = this.previousRate;
+    }else{
+      this.selectedRate = 0;
+    }
+  }
+
+  public rate(index: number){
+    this.selectedRate = index + 1;
+    this.previousRate = this.selectedRate;
   }
 
 }
