879f17e43e9d0d2b5774b8ed8c130862fb3565a0
majrbek kotto <dewdar@live.no>
2021-02-18 17:21:40 +0100

    testing after updating github account details

--- cinemahub/src/app/app.module.ts
+++ cinemahub/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//test
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesListComponent } from './movies/movies-list/movies-list.component';
