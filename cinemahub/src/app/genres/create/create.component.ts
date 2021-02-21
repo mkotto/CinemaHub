c5848d91481dc02aead2db39ee5d325fcb64be20
majrbek kotto <dewdar@live.no>
2021-02-21 21:08:34 +0100

    Genres form: set name field to required, added validator and error messages

--- cinemahub/src/app/genres/create/create.component.ts
+++ cinemahub/src/app/genres/create/create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,12 +14,27 @@ export class CreateComponent implements OnInit {
   form: FormGroup;
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      name: ''
+      name: ['',{
+        validators: [Validators.required, Validators.minLength(3)]
+      }]
     });
   }
 
   saveChanges(){
     this.router.navigate(['/genres']);
+  }
+
+  getErrorMessage(){
+    const field = this.form.get('name');
+    if(field.hasError('required')){
+      return 'The name filed is required';
+    }
+
+    if(field.hasError('minlength')){
+      return 'The name field must contain at least 3 characters'
+    }
+
+    return '';
   }
 
 }
