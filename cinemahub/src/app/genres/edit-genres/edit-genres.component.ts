3f118e2388e00fe5a6a05abfe9393f9fc7eb18d7
majrbek kotto <dewdar@live.no>
2021-02-23 21:48:30 +0100

    new form with checkbox, select and clear button

--- cinemahub/src/app/genres/edit-genres/edit-genres.component.ts
+++ cinemahub/src/app/genres/edit-genres/edit-genres.component.ts
@@ -23,7 +23,7 @@ export class EditGenresComponent implements OnInit {
     );
   }
 
-  onSaveChanges(){
+  saveChanges(genreCreationDTO?: genreCreationDTO){
 
   }
 
--- cinemahub/src/app/movies/movie-filter/movie-filter.component.ts
+++ cinemahub/src/app/movies/movie-filter/movie-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-filter',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieFilterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  form: FormGroup;
+  genres = [
+    { id: 1, name: 'Drama' },
+    { id: 2, name: 'Comedy' }
+  ];
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        title: '',
+        genreId: 0,
+        upComingReleases: false,
+        inTheaters: false
+      }
+    );
+  }
+
+  clearForm(){
+    
   }
 
 }
