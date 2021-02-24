6cb8586ed12c68a10742a54af9b855e948ecd766
majrbek kotto <dewdar@live.no>
2021-02-24 22:17:41 +0100

    Added some movies to filter

--- cinemahub/src/app/movies/movie-filter/movie-filter.component.ts
+++ cinemahub/src/app/movies/movie-filter/movie-filter.component.ts
@@ -16,6 +16,13 @@ export class MovieFilterComponent implements OnInit {
     { id: 2, name: 'Comedy' }
   ];
 
+  movies = [
+    { title: 'Dune', poster: 'https://m.media-amazon.com/images/M/MV5BMGFkZGY0NTgtMGEyZC00YzhjLTkyOWItYzMzOTljMDA3ZjU2XkEyXkFqcGdeQXVyNzc4NTU3Njg@._V1_UX182_CR0,0,182,268_AL_.jpg'},
+    { title: 'Fifth Element', poster: 'https://m.media-amazon.com/images/M/MV5BZWFjYmZmZGQtYzg4YS00ZGE5LTgwYzAtZmQwZjQ2NDliMGVmXkEyXkFqcGdeQXVyNTUyMzE4Mzg@._V1_UY268_CR2,0,182,268_AL_.jpg'},
+    { title: 'Hot Fuzz', poster: 'https://m.media-amazon.com/images/M/MV5BMzg4MDJhMDMtYmJiMS00ZDZmLThmZWUtYTMwZDM1YTc5MWE2XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg'},
+    { title: 'Snatch', poster: 'https://m.media-amazon.com/images/M/MV5BMTA2NDYxOGYtYjU1Mi00Y2QzLTgxMTQtMWI1MGI0ZGQ5MmU4XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_UY268_CR0,0,182,268_AL_.jpg'}
+  ];
+
   ngOnInit(): void {
     this.form = this.formBuilder.group(
       {
