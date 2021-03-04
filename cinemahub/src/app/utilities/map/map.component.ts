7ad4015d41258ec626552823677701b983e09f4e
majrbek kotto <dewdar@live.no>
2021-03-04 22:24:48 +0100

    added marker to map

--- cinemahub/src/app/utilities/map/map.component.ts
+++ cinemahub/src/app/utilities/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { tileLayer, latLng } from 'leaflet';
+import { tileLayer, latLng, LeafletMouseEvent, Marker, marker } from 'leaflet';
 // import { Map, Control, DomUtil, ZoomAnimEvent , Layer, MapOptions, tileLayer, latLng } from 'leaflet';
 // import L from 'leaflet';
 @Component({
@@ -13,11 +13,23 @@ export class MapComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  
+
+  layers: Marker<any>[] = [];
   options = {
     layers: [
       tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'Angular movies' })
     ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909)
+    zoom: 14,
+    center: latLng(43.31886889587862 , 45.693168640136726)
   };
+
+  handleMapClick(event: LeafletMouseEvent) {
+    const latitude = event.latlng.lat;
+    const longtitude = event.latlng.lng;
+    console.log(latitude + ' / ' + longtitude);
+    this.layers = [];
+    this.layers.push(marker([latitude, longtitude]));
+  }
 }
