0ef1ffbf90854968fc9a17dbff0786ffa6636716
majrbek kotto <dewdar@live.no>
2021-03-12 21:46:38 +0100

    custom validation

--- MoviesAPI/Entities/Genre.cs
+++ MoviesAPI/Entities/Genre.cs
@@ -1,13 +1,28 @@
+using MoviesAPI.Validations;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace MoviesAPI.Entities
 {
-  public class Genre
+  public class Genre : IValidatableObject
   {
     public int Id { get; set; }
     [Required(ErrorMessage = "The field with name {0} is required")]
     [MinLength(3)]
     [MaxLength(20)]
+    //[FirstLetterUppercase]
     public string Name { get; set; }
-  }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                var firstLetter = Name[0].ToString();
+                if(firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("First letter must be uppercase", new string[] { nameof(Name) });
+                }
+            }
+        }
+    }
 }
