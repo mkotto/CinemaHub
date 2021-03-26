deb0501a2469b15e229e9b97179339c0ad63026a
majrbek kotto <dewdar@live.no>
2021-03-26 22:31:09 +0100

    new dto classes

---
+++ MoviesAPI/DTOs/GenreCreationDTO.cs
@@ -0,0 +1,19 @@
+﻿using MoviesAPI.Validations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.DTOs
+{
+    public class GenreCreationDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "The field with name {0} is required")]
+        [MinLength(3)]
+        [MaxLength(20)]
+        [FirstLetterUppercase]
+        public string Name { get; set; }
+    }
+}
