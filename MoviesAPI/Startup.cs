9cebcaa7ce469753a3a0c073599453eb91084fc5
Mairbek Tungaev <42913994+mkotto@users.noreply.github.com>
2021-04-02 19:54:10 +0200

    copied code without knowing what it event means, without any guilt whatsoever

--- MoviesAPI/Startup.cs
+++ MoviesAPI/Startup.cs
@@ -2,15 +2,21 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using MoviesAPI.Entities;
 using MoviesAPI.Filters;
 using MoviesAPI.Services;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MoviesAPI
 {
@@ -26,51 +32,68 @@ namespace MoviesAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddCors(
-                    options =>
-                    {
-                        options.AddDefaultPolicy( builders => {
-                            var frontendURL = Configuration.GetValue<string>("frontend_url");
-                            builders.WithOrigins(frontendURL).AllowAnyMethod().AllowAnyHeader();
-                        });
-                        options.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
-                    }
-                );
-            services.AddAutoMapper(typeof(Startup));
-            services.AddControllers(
-                    options =>
-                    {
-                        options.Filters.Add(typeof(CustomExceptionFilter));
-                    }
-                );
-            services.AddMemoryCache();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
-            //services.AddMvcCore();
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(CustomExceptionFilter));
+            });
+
+            services.AddResponseCaching();
             services.AddSingleton<IRepository, InMemoryRepository>();
             services.AddTransient<ActionFilters>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoviesAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+            app.Use(async (context, next) =>
+            {
+                using (var swapStream = new MemoryStream())
+                {
+                    var originalResponseBody = context.Response.Body;
+                    context.Response.Body = swapStream;
+
+                    await next.Invoke();
+
+                    swapStream.Seek(0, SeekOrigin.Begin);
+                    string responseBody = new StreamReader(swapStream).ReadToEnd();
+                    swapStream.Seek(0, SeekOrigin.Begin);
+
+                    await swapStream.CopyToAsync(originalResponseBody);
+                    context.Response.Body = originalResponseBody;
+
+                    logger.LogInformation(responseBody);
+                }
+            });
+
+            app.Map("/map1", (app) =>
+            {
+                app.Run(async context =>
+                {
+                    await context.Response.WriteAsync("I'm short-circuiting the pipeline");
+                });
+            });
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoviesAPI v1"));
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseCors(
-                    x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
-                );
-            app.UseHttpsRedirection();
+
             app.UseResponseCaching();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
